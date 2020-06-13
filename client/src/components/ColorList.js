c5e2664f06b3f2262411f05571c39bc193f85e08
Dominique Garrett <dmnqgrrtt@gmail.com>
2020-06-12 23:05:22 -0400

    update colors now working

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,6 +21,20 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res=>{
+        console.log("put res", res)
+        updateColors(colors.map(color=>{
+          if(color.id === colorToEdit.id) {
+            return res.data;
+          }
+          return color;
+        }))
+        setEditing(false);
+
+      })
+      .catch(err=>console.log(err))
   };
 
   const deleteColor = color => {
